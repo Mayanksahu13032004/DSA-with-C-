19d80b54e8e45d1e076600bce00d475988470b38
Mayanksahu13032004 <mayanksahu13032004.com>
2024-05-10 07:03:45 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=123;
+    cout<<"Hello world\n";
+    cout<<a<<endl;
+
+    char ch='m';
+    // only single charater likhana hai in char
+    cout<<ch <<endl;
+ 
+   int size=sizeof(a);
+    cout <<"size of a is" <<size<<endl;
+}
